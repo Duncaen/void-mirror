f1805a7d546c7fa7d94ce7bb9425328a2495e978
Duncan Overbruck <mail@duncano.de>
2023-05-22 21:58:53 +0200

    move download to own file

--- main.go
+++ main.go
@@ -291,14 +291,14 @@ type Repository struct {
 	Stagedata *Stagedata
 	ticker    *time.Ticker
 	req       *http.Request
-	downloads map[*DownloadFile]struct{}
+	downloads map[*Download]struct{}
 	files     map[string]struct{}
 	obsolete  map[string]time.Time
 	results   chan DownloadResult
 	ctx       context.Context
 }
 
-func (r *Repository) queue(download DownloadFile) {
+func (r *Repository) queue(download Download) {
 	dl := &download
 	r.downloads[dl] = struct{}{}
 	wp.Submit(func() {
@@ -309,14 +309,14 @@ func (r *Repository) queue(download DownloadFile) {
 
 func (r *Repository) queuePkg(pkg *pkg) {
 	binpkg := pkg.Filename()
-	r.queue(DownloadFile{
+	r.queue(Download{
 		URL:       r.Config.Upstream.JoinPath(binpkg),
 		Filename:  binpkg,
 		Directory: r.Config.Destination,
 		SHA256:    pkg.SHA256,
 	})
 	sigfile := binpkg + ".sig"
-	r.queue(DownloadFile{
+	r.queue(Download{
 		URL:       r.Config.Upstream.JoinPath(sigfile),
 		Filename:  sigfile,
 		Directory: r.Config.Destination,
@@ -328,7 +328,7 @@ func NewRepository(ctx context.Context, config *config.RepositoryConfig) (*Repos
 		Config: config,
 		files:     make(map[string]struct{}),
 		obsolete:  make(map[string]time.Time),
-		downloads: make(map[*DownloadFile]struct{}),
+		downloads: make(map[*Download]struct{}),
 		results:   make(chan DownloadResult),
 		ctx:       ctx,
 	}
@@ -355,7 +355,7 @@ func NewRepository(ctx context.Context, config *config.RepositoryConfig) (*Repos
 			if !os.IsNotExist(err) {
 				return nil, err
 			}
-			r.queue(DownloadFile{
+			r.queue(Download{
 				URL:       config.Upstream.JoinPath(binpkg),
 				Filename:  binpkg,
 				Directory: config.Destination,
@@ -367,7 +367,7 @@ func NewRepository(ctx context.Context, config *config.RepositoryConfig) (*Repos
 			if !os.IsNotExist(err) {
 				return nil, err
 			}
-			r.queue(DownloadFile{
+			r.queue(Download{
 				URL:       config.Upstream.JoinPath(sigfile),
 				Filename:  sigfile,
 				Directory: config.Destination,
@@ -433,106 +433,6 @@ func (r *Repository) Run(ctx context.Context) error {
 			return ctx.Err()
 		}
 	}
-}
-
-type DownloadResult struct {
-	DownloadFile *DownloadFile
-	err          error
-}
-
-type DownloadFile struct {
-	URL       *url.URL
-	Filename  string
-	Directory string
-	SHA256    []byte
-}
-
-func (dl *DownloadFile) Download(ctx context.Context, req *http.Request) (err error) {
-	req = req.Clone(ctx)
-	req.URL = dl.URL
-	slog.Debug("downloading", slog.Group("req", "url", req.URL))
-	var resp *http.Response
-	resp, err = http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	defer func() {
-		attr := []any{
-				slog.Group("req", "url", req.URL),
-				slog.Group("resp", "status", resp.StatusCode),
-		}
-		if err != nil {
-			attr = append(attr, slog.Any("error", err))
-			slog.Info("download failed", attr...)
-		} else {
-			slog.Info("download finished", attr...)
-		}
-	}()
-
-	switch resp.StatusCode {
-	case http.StatusNotModified:
-		return nil
-	default:
-		slog.Error("unexpected status code", slog.Group("req", "url", req.URL), slog.Group("resp", "status", resp.Status))
-		return nil
-	case http.StatusOK:
-		// fallthrough
-	}
-
-	var rd io.Reader
-	rd = resp.Body
-	var hash hash.Hash
-	if len(dl.SHA256) != 0 {
-		hash = sha256.New()
-		rd = io.TeeReader(rd, hash)
-	}
-
-	pattern := fmt.Sprintf(".%s.*", dl.Filename)
-	tmpfile, err := Tempfile(dl.Directory, pattern)
-	if err != nil {
-		slog.Error("could not create tmp file", "directory", dl.Directory, "error", err)
-		return err
-	}
-	defer tmpfile.Remove()
-	tmpfilename := tmpfile.file.Name()
-
-	_, err = io.Copy(tmpfile.file, rd)
-	if err != nil {
-		slog.Error("writing tmpfile failed", "path", tmpfilename, "error", err)
-		return err
-	}
-	if err = resp.Body.Close(); err != nil {
-		slog.Error("closing response body failed", "error", err)
-		err = nil
-	}
-
-	if hash != nil {
-		sum := hash.Sum(nil)
-		if !bytes.Equal(sum[:], dl.SHA256) {
-			slog.Error("hash mismatch", "expected", hex.EncodeToString(dl.SHA256), "got", hex.EncodeToString(sum))
-			return fmt.Errorf("hash mismatch")
-		}
-	}
-
-	// set the modtime on the temporary file
-	if value := resp.Header.Get("Last-Modified"); value != "" {
-		var date time.Time
-		if date, err = time.Parse(http.TimeFormat, value); err == nil {
-			if err := os.Chtimes(tmpfilename, time.Now(), date); err != nil {
-				slog.Error("changing mod time failed", "path", tmpfilename, "error", err)
-				err = nil
-			}
-		}
-	}
-
-	// everything fine, rename the temporary file
-	destfile := filepath.Join(dl.Directory, dl.Filename)
-	if err = tmpfile.Commit(filepath.Join(dl.Directory, dl.Filename)); err != nil {
-		slog.Error("renaming tempfile", "oldpath", tmpfilename, "newpath", destfile, "error", err)
-		return err
-	}
-	return nil
 }
 
 var wp *workerpool.WorkerPool
